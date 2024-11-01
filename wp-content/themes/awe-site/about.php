c2f6fa07ab44e241cec7a00e7b71c3736b10a667
Gabriel-P-Freitas <f.pessoa@escolar.ifrn.edu.br>
2024-10-31 21:15:55 -0300

    theme awe-site

---
+++ wp-content/themes/awe-site/about.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/global.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+        integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="assets/css/about.css">
+    <title>Quem somos</title>
+</head>
+<body>
+    <header> 
+        <div class="equalizator"></div>
+        <nav class="navbar container-menu py-3 fixed-top">
+            <div class="container">
+                <a class="navbar-brand" href="#"><img src="assets/imgs/logo.svg" alt="logo AWE"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
+                    data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon light"></span>
+                </button>
+                <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar"
+                    aria-labelledby="offcanvasNavbarLabel">
+                    <div class="offcanvas-header">
+                        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div class="offcanvas-body">
+                        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                    aria-expanded="false">
+                                    Dropdown
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <form class="d-flex mt-3" role="search">
+                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <section class="quem-somos">
+        <div class="quem-somos-text">
+            <p>QUEM SOMOS</p>
+            <h2>Nosso Projeto</h2>
+        </div>
+        <div class="projeto">
+            <div class="inquietos">
+            <h3>Somos inquietos e queremos inovar na escola</h3>
+            <p>O projeto visa acelerar o desenvolvimento das habilidades técnicas e interpessoais dos alunos colocando eles em um ambiente de imersão. O foco está em auxiliar no desenvolvimento de profissionais mais preparados para um mercado exigente e em constante mudança, por isso é preciso trabalhar, além de habilidades técnicas, as habilidades interpessoais.</p>
+        </div>
+        <div class="surgiu">
+            <h3>Por que surgiu?</h3>
+            <p>No cenário atual, onde a escola anda num descompasso comparado ao mundo contemporâneo, a AWE busca ser o elo entre a escola e o mercado de trabalho. Buscamos aliar conhecimentos técnicos (hard skills) e habilidades interpessoais (soft skills) em um ambiente desafiador que lida diretamente com resolução de problemas reais. Todos os problemas surgem de clientes parceiros.</p>
+        </div>
+        <div class="guia">
+            <h3>O que nos guia?</h3>
+            <p>Assim como no mercado de trabalho, muito do que aprendemos aqui é a partir de desafios práticos que fazem nos movermos em direção às  soluções inovadoras. </p>
+            <div class="tags">
+                <p>#propósitos</p>
+                <p>#afinidades</p>
+                <p>#autonomia</p>
+                <p>#mestria</p>
+            </div>
+        </div>
+        </div>
+        <div class="pilares">
+            <h2>Nossos pilares</h2>
+            <div class="propositos">
+                <h3>Propósitos</h3>
+                <p>Acreditamos que tudo que fazemos precisa ser relevante para nós, a um grupo e à sociedade. Por isso buscamos aliar os anseios dos alunos, com os objetivos da <span class="destaque-awe">AWE</span> e a contribuição para a sociedade.</p>
+            </div>
+            <div class="autonomia">
+                <h3>Autonomia</h3>
+                <div class="autonomia-text">
+                    <p>Incentivamos todos a se tornarem <span class="destaque-fundo"> autogestionáveis.</span></p>
+                    <div class="quebra-linha"></div>
+                    <p>As pessoas querem dirigir as próprias vidas e na AWE buscamos fortalecer isso, seja por meio de mentorias individiduais, trabalho colaborativo ou atráves de cursos.</p>
+                </div>
+            </div>
+            <div class="afinidade">
+                <h3>Afinidade</h3>
+                <div class="afinidade-text">
+                    <p>O trabalho em equipe não se restringe ao espaço da <span class="destaque-awe">AWE</span>, mas é essencial para toda vida em sociedade.</p>
+                    <div class="quebra-linha"></div>
+                    <p>Por isso entendemos e compartilhamos a ideia de pensamento sistêmico, onde cada pessoa é parte de algo maior e que todos juntos somos capazes de construir grandes coisas. O trabalho em grupo é uma habilidade trabalhada constantemente, desde a concepção de problemas simples às decisões mais impactantes.</p>
+                </div>
+            </div>
+            <div class="mestria">
+                <h3>Mestria</h3>
+                <div class="mestria-text">
+                    <p><span class="destaque-fundo">Os desafios nos convidam a pesquisar e aprender mais.</span></p>
+                    <div class="quebra-linha"></div>
+                    <p>Buscamos impactar e sempre trazemos e demonstramos conhecimento aprofundado sobre determinada habilidade para solucionar os problemas dos clientes. </p>
+                    <div class="quebra-linha"></div>
+                    <p>Faz parte de nós: pesquisar, aprender, demonstrar, solucionar e compartilhar as habilidades aprendidas durante nosso percurso.</p>
+                </div>
+            </div>
+        </div>
+        
+    </section>
+    <footer>
+        <section class="contact-instagram">
+            <div class="container flex-column d-flex align-items-center gap-3 py-5">
+                <span class="link-instagram d-flex gap-2">
+                    <a href="https://www.instagram.com/awescolar.ifrn?igsh=b2kzdGhlZWMxbWll" target="_blank">
+                        <i class="fa-brands fa-instagram" style="color: #0b5abd;"></i>
+                    </a>
+                    <a href="https://www.instagram.com/awescolar.ifrn?igsh=b2kzdGhlZWMxbWll" target="_blank"
+                        class="fst-italic">@awescolar</a>
+                </span>
+                <span class="text-center fst-italic">segue a gente lá no instagram, dá essa moralzinha pra nós!</span>
+            </div>
+        </section>
+        <section class="content-footer">
+            <div class="container pt-5 d-flex flex-column gap-5 px-4">
+                <p class="text"><span>Queremos você aqui</span>. Que tal você fazer parte da nossa equipe e mudarmos o mundo,
+                    <span>juntos</span>?</p>
+                <section class="links-contatcs d-flex gap-4 row">
+                    <div class="email links d-flex flex-column">
+                        <span class="title-info">email</span>
+                        <span>awescolar@gmail.com</span>
+                    </div>
+                    <div class="address links d-flex flex-column">
+                        <span class="title-info">endereço</span>
+                        <span>FRN campus Natal Natal Zona Norte
+                            Rua Brusque, 2926, Potengi | Natal-RN</span>
+                    </div>
+                    <div class="media links d-flex flex-column gap-2">
+                        <span class="title-info">nossas mídias</span>
+                        <ul class="d-flex gap-4 align-items-center">
+                            <li><a href="#"><i class="fa-brands fa-instagram" style="color: #8FC1FF;"></i></a></li>
+                            <li><a href="#"><i class="fa-brands fa-dribbble" style="color: #8FC1FF;"></i></a></li>
+                            <li><a href="#"><i class="fa-brands fa-behance" style="color: #8FC1FF;"></i></a></li>
+                            <li><a href="#"><img src="assets/imgs/icon_github.svg" alt="logo github" style="color: #8FC1FF;"></a></li>
+                            <li><a href="#"><img src="assets/imgs/icon_linkedin.svg" alt="icon linkedin" style="color: #8FC1FF;"></a></li>
+                        </ul>
+                    </div>
+                </section>
+            </div>
+        </section>
+        <div class="stand-logo py-4 mx-4 d-flex justify-content-center">
+            <img src="assets/imgs/logo-v2.svg" alt="logo v2">
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+        integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
+        crossorigin="anonymous"></script>
+
+</body>
+
+</html>
