49bd57596b83c58336cccbd1cdb16ee285f403e4
Gabriel-P-Freitas <f.pessoa@escolar.ifrn.edu.br>
2024-11-11 13:00:30 -0300

    configurando home

--- wp-content/themes/awe-theme/index.php
+++ wp-content/themes/awe-theme/index.php
@@ -267,7 +267,7 @@
                         ?>
                     </section>
                 </div>
-                <button class="text-uppercase d-flex align-items-center mt-4 fw-bold ms-auto btn-1">Acesse mais notícias <img
+                <button class="text-uppercase d-flex align-items-center mt-4 fw-bold btn-1">Acesse mais notícias <img
                         src="<?php echo get_template_directory_uri() ?>/assets/imgs/arrow-right-blue-dark.svg" alt="arrow-right-blue"></button>
             </div>
         </section>
